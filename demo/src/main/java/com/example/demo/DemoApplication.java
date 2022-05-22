ffb3bb9c705f7057f801f1b14c6461dfdbe49fac
siddhartha sarkar <siddhartha.sarkar.guest@saipem.com>
2022-05-23 01:37:08 +0530

    release

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -11,3 +11,15 @@ public class DemoApplication {
 	}
 
 }
+
+package com.technicalkeeda.app;
+ 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
